e9f6f6786c1f5830b8537696f83f657b8c7be5bb
Rnely <ricards.girza@gmail.com>
2023-03-02 15:51:38 +0200

    Base project with cleaned up code and a home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      <p>SpaceTech Project</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+
+    return (
+        <p>SpaceTech Project</p>
+    )
+}
+
+export default Home;
